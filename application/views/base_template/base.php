35918735f5c683876bfc3498cfba7ad7c2f11719
dfkuro <izmirreffi@gmail.com>
2016-12-25 03:04:09 -0600

    Coding home Layout From Scratch
    Coding the home section

--- application/views/base_template/base.php
+++ application/views/base_template/base.php
@@ -8,8 +8,30 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="<?=base_url( 'assets/favicon.ico' )?>">
+    <link rel="stylesheet" href="<?=base_url('resources/font_awesome/css/font-awesome.min.css')?>">
 
     <title>Love Academy</title>
+
+    <style media="screen">
+
+      /*TODO: Add font at last*/
+      @font-face {
+        font-family: myFirstFont;
+        src: url(sansation_light.woff);
+      }
+
+      .item-menu-icon{
+        margin: 12px;
+        padding: 12px;
+        color: #EC2587;
+      }
+
+      .item-menu-link{
+        color: #000000;
+        padding-top: 26px;
+        font-size: 1.2em;
+      }
+    </style>
 
     <!-- Bootstrap core CSS -->
     <link href="<?=base_url( 'vendor/twbs/bootstrap/dist/css/bootstrap.min.css?ver=' ).rand()?>" rel="stylesheet">
@@ -67,16 +89,18 @@
             <div class="cabecera" style="height: 100px;">
 
               <div class="logo pull-left">
-                <img src="" alt="">
-                <h2 style="color: #ff1188; font-weight: 600;" >LOVE ACADEMY</h2>
+
+                <h2 style="color: #EC2587; font-weight: 600;" ><i class="fa fa-heart pull-left"></i> LOVE ACADEMY</h2>
               </div>
 
-              <div class="social pull-right">
-
+              <div class="social pull-right" style="margin: 30px;">
+                <i class="fa fa-facebook"></i>
+                <i class="fa fa-vine"></i>
+                <i class="fa fa-youtube"></i>
               </div>
 
-              <div class="login pull-right">
-
+              <div class="login pull-right" style="margin: 30px;">
+                SIGN UP / LOG ING
               </div>
 
             </div>
@@ -85,24 +109,220 @@
 
 
       <div class="row">
+
+        <!-- Header and menu section -->
         <div class="col-xs-10 col-xs-offset-1">
 
           <div class="row">
             <div class="col-xs-12">
               <div class="contenido-sistema" style="height: 400px; background-color: red;">
+                <img src="http://placehold.it/945x400" alt="" class="img img-responsive">
 
+                <!-- TODO: Need fix on space, fix separator '-', center items on the menu -->
+                <div class="" style="height: 70px; background-color: #CDCCCB; " id="menu">
+
+                  <div class="item-menu-icon pull-left">
+                    <i class="fa fa-camera fa-2x"></i>
+                  </div>
+
+                  <div class="item-menu-link pull-left">
+                    <p> ABOUT US - </p>
+                  </div>
+
+                  <div class="item-menu-link pull-left">
+                    <p> TEACHERS & HEALERS & COACHES - </p>
+                  </div>
+
+                  <div class="item-menu-link pull-left">
+                    <p> EXPERIENCIES - </p>
+                  </div>
+
+                  <div class="item-menu-link pull-left">
+                    <p> KNOWLEDGE - </p>
+                  </div>
+
+                  <div class="item-menu-link pull-left">
+                    <p> CALENDAR</p>
+                  </div>
+
+                </div>
               </div>
             </div>
           </div>
 
+          <!-- What is love academy and newsletter -->
+          <div class="row">
+
+            <div class="col-xs-8">
+              <img src="http://placehold.it/650x400" alt="" class="img-responsive">
+            </div>
+
+            <div class="col-xs-4">
+
+              <div class="" style="background: #000000; color: #ffffff; padding: 2px 10px 0px 10px;">
+                <h3>N E W S L E T T E R</h3>
+                <p>GREAT DISCOUNTS, FREE LECTURES AND  CONFERENCES, DOWNLOADS AND MORE... </p>
+              </div>
+
+              <!-- TODO: Fix space between inputs -->
+              <form>
+                <div class="form-group">
+                  <label for="form_name">NAME</label>
+                  <input type="text" class="form-control" id="form_name" placeholder="">
+                  <div class="form-group pull-right" style="padding-top: 7px;">
+                    <select class="form-control" id="sel1" style="font-size: 0.8em; height: 26px;">
+                      <option>MASCULINE</option>
+                      <option>MASCULINE 2</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="form_birthday">BIRTHDAY</label>
+                  <input type="date" class="form-control" id="form_birthday" placeholder="">
+                </div>
+
+                <div class="form-group">
+                  <label for="form_country">COUNTRY / CITY</label>
+                  <input type="text" class="form-control" id="form_country" placeholder="">
+                </div>
+
+                <div class="form-group">
+                  <label for="form_email">E-MAIL</label>
+                  <input type="email" class="form-control" id="form_email" placeholder="">
+                </div>
+
+
+                <!-- TODO: Add style to the button then make it a css rule -->
+                <button type="submit" class="btn btn-default pull-right" >SUSCRIBE</button>
+              </form>
+
+            </div>
+
+          </div>
+
+          <!-- Section: TEACHERS & HEALERS & COACHES -->
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="" style="background-color: #C7C6C2; height: 40px;">
+                <h2 class="text-center" style="color: #EC2587; padding-top: 4px;">TEACHERS & HEALERS & COACHES</h2>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <!-- TODO: Need to fix thumnails -->
+          <div class="row">
+            <div class="col-xs-4">
+              <div class="thumbnail" style="padding: 10px; height: 250px;">
+                <img src="https://1.bp.blogspot.com/-aFQ-W_KTFWQ/V6BdtpSUy6I/AAAAAAAAAH4/xD_U-BYItSsNvk1UGfROqLBzzU1h32oXQCLcB/s320/4-diwali-greeting-cards-by-ajay-acharya.jpg" alt="Bootstrap Thumbnail: Beautiful Bootstrap Thumbnail like Material Design Cards">
+                <div class="caption">
+                  <div class="" style="background-color: #C7C6C2; height: 25px;">
+                    <h4 class="text-center" style="color: #ffffff; padding: 4px;">SASHA COBRA</h4>
+                    <div class="teacher-info" style="padding: 5px; height: 160px;">
+                      <p style="font-size: 0.65em;">
+                        Sasha has been living, training and working with Shantam Nityama for over 8 years and is one of three certified Nitvana Bodywork Practitioners in the world. She now travels globally, sharing the gifts of this work through private sessions and workshops.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-4">
+              <div class="thumbnail" style="padding: 10px; height: 250px;">
+                <img src="https://1.bp.blogspot.com/-aFQ-W_KTFWQ/V6BdtpSUy6I/AAAAAAAAAH4/xD_U-BYItSsNvk1UGfROqLBzzU1h32oXQCLcB/s320/4-diwali-greeting-cards-by-ajay-acharya.jpg" alt="Bootstrap Thumbnail: Beautiful Bootstrap Thumbnail like Material Design Cards">
+                <div class="caption">
+                  <div class="" style="background-color: #C7C6C2; height: 25px;">
+                    <h4 class="text-center" style="color: #ffffff; padding: 4px;">SASHA COBRA</h4>
+                    <div class="teacher-info" style="padding: 5px; height: 160px;">
+                      <p style="font-size: 0.65em;">
+                        Sasha has been living, training and working with Shantam Nityama for over 8 years and is one of three certified Nitvana Bodywork Practitioners in the world. She now travels globally, sharing the gifts of this work through private sessions and workshops.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-4">
+              <div class="thumbnail" style="padding: 10px; height: 250px;">
+                <img src="https://1.bp.blogspot.com/-aFQ-W_KTFWQ/V6BdtpSUy6I/AAAAAAAAAH4/xD_U-BYItSsNvk1UGfROqLBzzU1h32oXQCLcB/s320/4-diwali-greeting-cards-by-ajay-acharya.jpg" alt="Bootstrap Thumbnail: Beautiful Bootstrap Thumbnail like Material Design Cards">
+                <div class="caption">
+                  <div class="" style="background-color: #C7C6C2; height: 25px;">
+                    <h4 class="text-center" style="color: #ffffff; padding: 4px;">SASHA COBRA</h4>
+                    <div class="teacher-info" style="padding: 5px; height: 160px;">
+                      <p style="font-size: 0.65em;">
+                        Sasha has been living, training and working with Shantam Nityama for over 8 years and is one of three certified Nitvana Bodywork Practitioners in the world. She now travels globally, sharing the gifts of this work through private sessions and workshops.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="" style="background-color: #C7C6C2; height: 40px;">
+                <h2 class="text-center" style="color: #EC2587; padding-top: 4px;">EXPERIENCES</h2>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="" style="background-color: #C7C6C2; height: 40px;">
+                <h2 class="text-center" style="color: #EC2587; padding-top: 4px;">SIGNATURE EVENTS</h2>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="" style="background-color: #C7C6C2; height: 40px;">
+                <h2 class="text-center" style="color: #EC2587; padding-top: 4px;">KNOWLEDGE</h2>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="" style="background-color: #C7C6C2; height: 40px;">
+                <h2 class="text-center" style="color: #EC2587; padding-top: 4px;">CALENDAR</h2>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="" style="background-color: #C7C6C2; height: 40px;">
+                <h2 class="text-center" style="color: #EC2587; padding-top: 4px;">PLAYLIST LOVE ACADEMY</h2>
+              </div>
+            </div>
+          </div>
+
+
         </div>
+
+
+
       </div>
 
 
 
     </div>
 
-    <div class="container" style="background-color: #fff;">
-      <button type="button" class="btn btn-primary" name="button"> Hi there! </button>
+    <!-- <div class="container" style="background-color: #fff;"> -->
+
   </body>
+
+  <!-- <script src="<?=base_url('resources')?>" type="text/javascript"></script> -->
 </html>
